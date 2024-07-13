067802e492116aa6f6e0c827c72333808b865ac6
bhumika-ios <bhumika.iosdeveloper.com>
2024-07-13 11:13:18 +0530

    Initial Commit

---
+++ DynamicHeight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicHeight
+//
+//  Created by Bhumika Patel on 13/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
